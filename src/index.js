752d555dd337c22619ddd531cae34268ee7dd27c
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2018-10-06 16:05:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  var noz = Number.MAX_VALUE;
+  let j = base;
+  let p=0;
+  for (var i = 2; i <= base; i++) {
+    if (j % i === 0) {
+         j = j / i;     
+         p++;
+      }
+      let c = 0;
+      let z = Math.floor(number / i);
+      while (z > 0) {
+        c += z;
+        z = Math.floor(z / i);
+      }
+      noz = Math.min(noz, Math.floor(c / p))
+    }
+  
+  return noz;
 }
